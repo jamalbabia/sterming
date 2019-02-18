04294753d03a3d15b4e58d2be913fa07a44d299e
jamalbabia <47610404+jamalbabia@users.noreply.github.com>
2019-02-18 13:32:37 -0800

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,7 @@
+﻿const Discord = require("discord.js");
+const client = new Discord.Client();
+client.on('ready', () => {
+  client.user.setGame(`help |^^invite^^`,'https://www.twitch.tv/By:A7MD');
+  console.log('BOT ONLINE');
+});
+client.login("NTQ1MDA2MjM0NjI2MDMxNjE4.D0cMHw.mSjIlEblNOFincgUaeIw6-tX6oA");
